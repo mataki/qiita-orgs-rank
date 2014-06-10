369a7c0b7fd0ff6237a1f6ef1b313c00ae2e2c81
Akihiro Matsumura <matsumura.aki@gmail.com>
2014-06-10 13:41:52 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QiitaOrgsRank::Application.config.secret_key_base = 'a8cdbc753a97493fc0d5c8371fafe8b90cb4cc762e4fa81fd869a7a094fd89da61af79680988aa4c032a863ab1e64e5166f8ec477b9a7582bfe61fc7adda4ab4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QiitaOrgsRank::Application.config.session_store :cookie_store, key: '_qiita-orgs-rank_session'
