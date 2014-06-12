a798bbb9b7f10767fe3caad4cdfc5536ab9462be
Akihiro Matsumura <matsumura.aki@gmail.com>
2014-06-12 18:47:20 +0900

    change count column to integer

--- db/migrate/20140610121617_create_members.rb
+++ db/migrate/20140610121617_create_members.rb
@@ -4,8 +4,8 @@ class CreateMembers < ActiveRecord::Migration
       t.belongs_to :organization, index: true
       t.string :name
       t.string :slug
-      t.string :post_count
-      t.string :stock_count
+      t.integer :post_count
+      t.integer :stock_count
 
       t.timestamps
     end
