09d76982afe81e9212199065d8b58edcc92470bc
Akihiro MATSUMURA <matsumura.aki@gmail.com>
2016-10-26 12:00:51 +0900

    Follow changing HTML

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,8 @@
 class Member < ActiveRecord::Base
   belongs_to :organization
 
+  validates :slug, presence: true
+
   def qiita_page
     "http://qiita.com/#{self.slug}"
   end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -11,7 +11,7 @@ class Organization < ActiveRecord::Base
 
     def fetch_organizations
       index_page = Mechanize.new.get('http://qiita.com/organizations')
-      index_page.search('.organizationsList h2 a').map do |a|
+      index_page.search('.organizationsList_orgName a').map do |a|
         Organization.find_or_create_by!(slug: a.attributes["href"].value.match(/\/organizations\/(.*)/)[1], name: a.text)
       end
     end
@@ -23,8 +23,8 @@ class Organization < ActiveRecord::Base
 
   def fetch
     org_page = Mechanize.new.get(qiita_page)
-    self.post_count = org_page.search('.organizationStatsSummary li:nth-child(1) .organizationStatsSummary_count').text.to_i
-    self.stock_count = org_page.search('.organizationStatsSummary li:nth-child(2) .organizationStatsSummary_count').text.to_i
+    self.post_count = org_page.search('.organizationHeader_stats .organizationHeader_stats_container:nth-child(1) .organizationHeader_stats_value').text.to_i
+    self.stock_count = org_page.search('.organizationHeader_stats .organizationHeader_stats_container:nth-child(2) .organizationHeader_stats_value').text.to_i
     self.save!
   rescue => e
     logger.info "[Error] fetch: #{self.id}"
@@ -33,13 +33,13 @@ class Organization < ActiveRecord::Base
   def fetch_members
     member_page = Mechanize.new.get("http://qiita.com/organizations/#{self.slug}/members")
     member_page.search('.organizationMemberList_item').map do |mdom|
-      a = mdom.search("a")
+      a = mdom.search(".organizationMemberList_memberProfile a")
       member = self.members.find_or_initialize_by(slug: a.text)
-      member.post_count, member.stock_count = mdom.search('.organizationMemberList_memberStats').text.scan(/\d+/)
+      member.post_count, member.stock_count = mdom.search('.organizationMemberList_memberStats').map{|d| d.text.scan(/\d+/).first }
       member.save!
       member
     end
   rescue => e
-    logger.info "[Error] fetch_members: #{self.id}"
+    logger.info "[Error] fetch_members: #{self.id} #{e}"
   end
 end
